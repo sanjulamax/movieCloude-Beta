77dfd05dbf91bf527ee15ca60ab972a9a3e3d4f4
SAVINDU ABEYWICKRAMA <sanjulasavindu@gmail.com>
2024-09-29 22:07:06 +0530

    complete

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -7,6 +7,7 @@ import yt from '../assets/yts.png'
 import li from '../assets/li23.png'
 import mail from '../assets/mail.png'
 import logo from '../assets/logo .png'
+import noResults from '../assets/search_no_result.png'
 
 
 
@@ -47,7 +48,7 @@ const Home = ()=>{
             </div>
             
    <div className='grid grid-cols-6 max-[430px]:grid max-[430px]:grid-cols-3 ' >
-              {noMovie ?<div className=' border-2 border-white w-screen h-screen flex items-start justify-center '> <div className='border-2 mt-20 border-white backdrop-blur-lg bg-white bg-opacity-10 rounded-lg '><img src="/src/assets/search_no_result.png" className='h-96  ' alt="no result found" /></div></div>:mvList.map((item)=>{
+              {noMovie ?<div className=' border-2 border-white w-screen h-screen flex items-start justify-center '> <div className='border-2 mt-20 border-white backdrop-blur-lg bg-white bg-opacity-10 rounded-lg '><img src={noResults} className='h-96  ' alt="no result found" /></div></div>:mvList.map((item)=>{
                          if(item.original_language === 'en'){
                             setEngPlayer(true)
                         }
@@ -56,15 +57,14 @@ const Home = ()=>{
                         }
      
                   return(
-                      <div className='max-[430px]:h-[500px]  h-4/5 pb-6 hover:border-2 hover:border-amber-300  items-center  border-2 border-white p-2 m-2  text-white bg-transparent backdrop-blur-xl rounded-md' key={item.id}>
+                      <div className='max-[430px]:h-[500px]  h-[600px] pb-6 hover:border-2 hover:border-amber-300  items-center  border-2 border-white p-2 m-2  text-white bg-transparent backdrop-blur-xl rounded-md' key={item.id}>
                
                             <img  className=' max-[430px]:h-1/3 w-full rounded-md h-1/2' src={`https://image.tmdb.org/t/p/w500${item.poster_path}`} alt={item.title} />
                             <h1 className=' w-full h-10 mt-2 overflow-hidden font-semibold text-2xl text-center '>{item.title}</h1>
                             <h2 className='text-center'>{item.release_date.slice(0,4)}</h2>
                             <p className='   max-[430px]:h-2/6 mb-8 overflow-hidden h-1/5 mt-2 rounded-md font-light text-sm text-start border-2 border-emerald-600 p-2'>{item.overview}</p>
                             <a className='absolute hover:bg-red-500  border-none  p-2  rounded-xl left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-blue-600 max-[430px]:pr-4 max-[430px]:pl-4 pl-6 pr-6'  href={`/player/${item.id}`}>Watch</a>
-                          {engPlayer?  <a className='absolute hover:bg-red-500  border-none  font-extralight  text-[15px] w-4/6 p-2  rounded-xl left-1/2 top-[95%] transform -translate-x-1/2 -translate-y-1/2 bg-blue-600 max-[430px]:pr-2 max-[430px]:pl-2 max-[430px]:text-[10px] max-[430px]:w-5/6 pl-4 pr-4'  href={`/EngPlayerPage/${item.id}`}>Watch Add Free</a>:null}
-                      </div>
+                            {engPlayer?  <a className='absolute hover:bg-red-500  border-none  font-extralight  text-[15px] w-4/6 p-2  rounded-xl left-1/2 top-[95%] transform -translate-x-1/2 -translate-y-1/2 bg-blue-600 max-[430px]:pr-2 max-[430px]:pl-2 max-[430px]:text-[10px] max-[430px]:w-5/6 pl-4 pr-4'  href={`/EngPlayerPage/${item.id}`}>Watch Add Free</a>:null}                      </div>
                   )
               })}
       
--- src/components/landing.jsx
+++ src/components/landing.jsx
@@ -25,12 +25,12 @@ const Landing = () => {
         <div className=' grid grid-cols-6 max-[430px]:grid max-[430px]:grid-cols-3  ' >
             {searchHider ? startupMvList.map((item)=>{
 
-if(item.original_language === 'en'){
-    setEngPlayer(true)
-}
-else{
-    setEngPlayer(false)
-}
+                    if(item.original_language === 'en'){
+                            setEngPlayer(true)
+                    }
+                    else{
+                            setEngPlayer(false)
+                    }
 
                 return(
                     <div className='max-[430px]:h-[550px]  h-[600px] pb-6 hover:border-2 hover:border-amber-300  items-center  border-2 border-white p-2 m-2  text-white bg-transparent backdrop-blur-xl rounded-md'  key={item.id}>
@@ -39,7 +39,7 @@ else{
                         <h2 className='text-center'>{item.release_date.slice(0,4)}</h2>
                         <p className=' mb-8 overflow-hidden max-[430px]:h-2/6 h-1/5 mt-2 rounded-md font-light text-sm text-start border-2 border-emerald-600 p-2'>{item.overview}</p>
                         <a className=' absolute hover:bg-red-500  border-none  p-2  rounded-xl left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-blue-600 max-[430px]:pr-4 max-[430px]:pl-4 pl-6 pr-6' href={`/player/${item.id}`}>Watch</a>
-                        {engPlayer?  <a className='absolute hover:bg-red-500  border-none  font-extralight  text-[15px] w-4/6 p-2  rounded-xl left-1/2 top-[95%] transform -translate-x-1/2 -translate-y-1/2 bg-blue-600 max-[430px]:pr-2 max-[430px]:pl-2 max-[430px]:text-[10px] max-[430px]:w-5/6 pl-4 pr-4'  href={`/EngPlayerPage/${item.id}`}>Watch Add Free</a>:null}
+                        {engPlayer? null : <a className='absolute hover:bg-red-500  border-none  font-extralight  text-[15px] w-4/6 p-2  rounded-xl left-1/2 top-[95%] transform -translate-x-1/2 -translate-y-1/2 bg-blue-600 max-[430px]:pr-2 max-[430px]:pl-2 max-[430px]:text-[10px] max-[430px]:w-5/6 pl-4 pr-4'  href={`/EngPlayerPage/${item.id}`}>Watch Add Free</a>}
 
                     </div>
                 )
